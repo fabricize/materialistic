76befc48dc4dd4406e3cd9e6b44956cf7712c117
Yasuaki Uechi <uetchy@randompaper.co>
2015-01-13 11:49:14 +0900

    Move Base

--- lib/materialistic/providers.rb
+++ lib/materialistic/providers.rb
@@ -1,7 +1,25 @@
-Dir[File.expand_path('../providers', __FILE__) << '/*.rb'].each do |file|
-  require file
+module Materialistic
+  module Providers
+    class Base
+      QUANTITY_UNKNOWN = 'QTY_UNKNOWN'
+      QUANTITY_HUGE = 'QTY_HUGE'
+
+      def id
+        underscore(self.class.to_s.gsub(/(^.+::)?/, ''))
+      end
+
+      private
+
+      def underscore(str)
+        str.gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+        gsub(/([a-z\d])([A-Z])/,'\1_\2').
+        tr("-", "_").
+        downcase
+      end
+    end
+  end
 end
 
-module Providers
-
+Dir[File.expand_path('../providers', __FILE__) << '/*.rb'].each do |file|
+  require file
 end

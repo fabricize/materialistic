61ec02606303a0e899a7d11e780d385edc231fed
Yasuaki Uechi <uetchy@randompaper.co>
2015-01-13 11:49:50 +0900

    Combine multiple result into one array

--- lib/materialistic.rb
+++ lib/materialistic.rb
@@ -5,11 +5,11 @@ module Materialistic
   def self.list(query)
     providers.inject([]) do |results, provider|
       context = provider.new
-      results << {
-        id: underscore(context.class.to_s.gsub(/(^.+::)?/, '')),
-        display_name: context.display_name,
-        items: context.list(query)
-      }
+      id = context.id
+      items = context.list(query)
+      items.each do |item|
+        results << item[:provider] = id
+      end
     end
   end
 
@@ -24,13 +24,6 @@ module Materialistic
 
   def self.providers
     ObjectSpace.each_object(Providers::Base.singleton_class).select{ |klass| klass.superclass == Providers::Base }
-  end
-
-  def self.underscore(str)
-    str.gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-    gsub(/([a-z\d])([A-Z])/,'\1_\2').
-    tr("-", "_").
-    downcase
   end
 
   def self.camelcase(str)
